22c19962ac3108ef10e58b38f81c759df180515c
Altrixity <altrixityyt@gmail.com>
2026-05-14 19:30:42 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener("fetch", event => {
   const url = new URL(event.request.url);
 
-  if (url.pathname.includes("/T3rr0r/proxy/")) {
-    const encoded = url.pathname.split("/T3rr0r/proxy/")[1];
+  if (url.pathname.includes("proxy/")) {
+    const encoded = url.pathname.split("proxy/")[1];
     const decoded = atob(encoded);
 
     event.respondWith(fetch(decoded, { mode: "cors" }));
